4a04ec255b9e68dde892015b79a8e2e2cf0ba6d8
Enrico Salapong <ejsalapong@csu.fullerton.edu>
2022-02-27 13:46:31 -0800

    added initial home screen on UI to get started with

--- pm/ui.py
+++ pm/ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk 
+from tkinter import ttk
 
 proj_name = 'Password Manager'
 
@@ -7,4 +8,24 @@ class MainWindow(tk.Tk):
         super().__init__(className=proj_name)
 
         self.title(proj_name) 
-        self.geometry("800x600")
+        self.geometry("600x300")
+        self.resizable(0,0)
+        self.attributes('-toolwindow', True)
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+
+        self.__create_widgets()
+
+    
+   
+#Initial layout just to get started; may need to organize into frames later on
+    def __create_widgets(self):
+        label1 = tk.Label(self, font=("Arial",25), text="Welcome")
+        label1.place(relx=.5, y= 50, anchor=tk.CENTER)
+
+        homebutton1 = tk.Button(self, text="New Database")
+        homebutton1.place(x=150, y= 200)
+
+        homebutton2 = tk.Button(self, text="Open Database")
+        homebutton2.place(x =350, y=200)
