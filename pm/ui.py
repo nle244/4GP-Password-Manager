50e52dfd285b663d2520e067a7c35fb5de5fcfaa
nle244 <nle244@csu.fullerton.edu>
2022-03-01 14:53:42 -0800

    "New Database" diag box complete

--- pm/ui.py
+++ pm/ui.py
@@ -1,5 +1,7 @@
 import tkinter as tk 
 from tkinter import ttk
+from tkinter.filedialog import asksaveasfile
+
 
 proj_name = 'Password Manager'
 
@@ -16,16 +18,19 @@ class MainWindow(tk.Tk):
         self.columnconfigure(1, weight=1)
 
         self.__create_widgets()
-
-    
    
 #Initial layout just to get started; may need to organize into frames later on
     def __create_widgets(self):
         label1 = tk.Label(self, font=("Arial",25), text="Welcome")
         label1.place(relx=.5, y= 50, anchor=tk.CENTER)
 
-        homebutton1 = tk.Button(self, text="New Database")
+        homebutton1 = tk.Button(self, text="New Database", command = newbuttonclick)
         homebutton1.place(x=150, y= 200)
 
         homebutton2 = tk.Button(self, text="Open Database")
         homebutton2.place(x =350, y=200)
+
+#UI Functions
+def newbuttonclick():
+    f = asksaveasfile(initialfile = 'newDatabase.txt', 
+        defaultextension = '.txt', filetypes = [("All Files","*.*")])
