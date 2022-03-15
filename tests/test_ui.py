e7115a9bb19f4ec8766e07028f4b4a4a179c8971
a.kim <dkim286@csu.fullerton.edu>
2022-03-15 01:37:50 -0700

    CPM-36 - add virtual display to work around GitHub being stupid

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,8 +1,17 @@
 import pytest
+import os
 from unittest.mock import patch
 from tkinter.ttk import Frame
 
 from pm.ui import MainWindow
+
+
+# workaround for github actions
+try:
+    disp = os.environ['DISPLAY']
+except KeyError:
+    os.system('Xvfb :1 -screen 0 1600x1200x16 &')
+    os.environ['DISPLAY'] = ':1.0'
 
 
 @pytest.fixture
