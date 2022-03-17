f305075129994f24ae6c8a8baade40165c4cce59
Enrico Salapong <ejsalapong@csu.fullerton.edu>
2022-03-17 14:24:22 -0700

    CPM-30 Edit and Delete buttons are now inactive when no rows are selected; streamlined code in Edit and Add buttons

--- pm/ui.py
+++ pm/ui.py
@@ -39,15 +39,27 @@ class MainWindow(ttk.Frame):
 
     #Initial layout just to get started; may need to organize into frames later on
     def __create_home_widgets(self):
-        self.label1 = ttk.Label(self, font=("Arial",25), text="Welcome")
+        self.label1 = ttk.Label(
+            self, 
+            font=("Arial",25), 
+            text="Welcome"
+            )
         self.label1.grid(row=0, column=0)
         #label1.place(relx=.5, y= 50, anchor=tk.CENTER)
 
-        self.homebutton1 = ttk.Button(self, text="New Database", command = self.newbuttonclick)
+        self.homebutton1 = ttk.Button(
+            self, 
+            text="New Database", 
+            command = self.newbuttonclick
+            )
         self.homebutton1.grid(row=1, column=0)
         #homebutton1.place(x=150, y= 200)
 
-        self.homebutton2 = ttk.Button(self, text="Open Database", command= self.load_filepath)
+        self.homebutton2 = ttk.Button(
+            self, 
+            text="Open Database", 
+            command= self.load_filepath
+            )
         self.homebutton2.grid(row=1, column=1)
         #homebutton2.place(x =350, y=200)
 
@@ -86,20 +98,19 @@ class MainWindow(ttk.Frame):
     #Creates the table view which will read data from the database file once decrypted for use
     def create_db_table(self, db):
         self.clear_frame()
-        test_table = ttk.Treeview(self)
-        test_table.grid(row=1, column=0, padx=20, pady=20)
-        test_table['columns'] = ('Title', 'Username', 'Password', 'URL', 'Last_Modified')
-        test_table['show'] = 'headings'
-        test_table.column("Title", anchor=tk.CENTER, width=50)
-        test_table.column("Username", anchor=tk.CENTER, width=80)
-        test_table.column("Password", anchor=tk.CENTER, width=80)
-        test_table.column("URL", anchor=tk.CENTER, width=100)
-        test_table.column("Last_Modified", anchor=tk.CENTER, width=100)
-        test_table.heading("Title", text="Title", anchor=tk.CENTER)
-        test_table.heading("Username", text="Username", anchor=tk.CENTER)
-        test_table.heading("Password", text="Password", anchor=tk.CENTER)
-        test_table.heading("URL", text="URL", anchor=tk.CENTER)
-        test_table.heading("Last_Modified", text="Last_Modified", anchor=tk.CENTER)
+        self.test_table = ttk.Treeview(self)
+        self.test_table['columns'] = ('Title', 'Username', 'Password', 'URL', 'Last_Modified')
+        self.test_table['show'] = 'headings'
+        self.test_table.column("Title", anchor=tk.CENTER, width=50)
+        self.test_table.column("Username", anchor=tk.CENTER, width=80)
+        self.test_table.column("Password", anchor=tk.CENTER, width=80)
+        self.test_table.column("URL", anchor=tk.CENTER, width=100)
+        self.test_table.column("Last_Modified", anchor=tk.CENTER, width=100)
+        self.test_table.heading("Title", text="Title", anchor=tk.CENTER)
+        self.test_table.heading("Username", text="Username", anchor=tk.CENTER)
+        self.test_table.heading("Password", text="Password", anchor=tk.CENTER)
+        self.test_table.heading("URL", text="URL", anchor=tk.CENTER)
+        self.test_table.heading("Last_Modified", text="Last_Modified", anchor=tk.CENTER)
 
         for row in db:
             title = row['Title']
@@ -107,24 +118,56 @@ class MainWindow(ttk.Frame):
             password = row['Password']
             url = row['URL']
             last_modified = row['Last_Modified']
-            test_table.insert("", tk.END, values=(title, username, password, url, last_modified))
+            self.test_table.insert("", tk.END, values=(title, username, password, url, last_modified))
 
         toolbar = ttk.Frame(self)
         eimg = PhotoImage(file='plus.png')
-        save_button = ttk.Button(toolbar, text="Save", width ="4",command= self.__save_database)
+
+        save_button = ttk.Button(
+            toolbar, 
+            text="Save", 
+            width ="4",
+            command= self.__save_database
+            )
         save_button.grid(row=0, column=0)
-        addButton = ttk.Button(toolbar, text="Add", image=eimg,width="3.5", command=self.add_entry) #Need a way to view icons to make it look nicer
+
+        addButton = ttk.Button(toolbar, 
+        text="Add", 
+        image=eimg,
+        width="3.5", 
+        command=self.add_entry
+        ) #Need a way to get "plus.png" to show to make it look nicer
         addButton.grid(row=0,column=1)
-        editButton = ttk.Button(toolbar, text="Edit" ,width="4", command= partial(self.edit_entry, test_table))
+
+        editButton = ttk.Button(
+            toolbar, 
+            text="Edit",
+            width="4", 
+            command= self.edit_entry,
+            state = DISABLED
+            )
         editButton.grid(row=0, column=2)
-        deleteButton = ttk.Button(toolbar, text="Delete" ,width="6", command= partial(self.delete_entry, test_table))
+
+        deleteButton = ttk.Button(
+            toolbar, 
+            text="Delete" ,
+            width="6", 
+            command= self.delete_entry,
+            state=DISABLED
+            )
         deleteButton.grid(row=0, column=3)
-        
-        
+
+        def enable_editdelete(event):
+            editButton['state'] = NORMAL
+            deleteButton['state'] = NORMAL
+
+        self.test_table.bind('<<TreeviewSelect>>', enable_editdelete)
+
         toolbar.grid(row=0,column=0)
+        self.test_table.grid(row=1, column=0, padx=20, pady=20)
         
 
-
+    
 
     #Initialize a new database and save to computer
     def newbuttonclick(self, *args):
@@ -148,13 +191,8 @@ class MainWindow(ttk.Frame):
         newwin = Toplevel(self)
         newwin.geometry("300x150")
         newwin.focus()
-        form_fields = {
-            "Title": "", 
-            "Username": "", 
-            "Password": "", 
-            "URL": "", 
-            "Last_Modified": ""
-        }
+        form_fields = dict()
+
         Label(newwin, text="Title").grid(row=0, column=0, padx=5)
         titleentry = Entry(newwin, width = 25)
         titleentry.grid(row=0, column=1, pady=5)
@@ -187,90 +225,70 @@ class MainWindow(ttk.Frame):
 
 
     #Edits specified row from the table and update it with new values
-    def edit_entry(self, Treeview):
-        try:
-            newwin = Toplevel(self)
-            newwin.geometry("300x150")
-            newwin.focus()
+    def edit_entry(self):
         
-            select = Treeview.selection()[0]
+        newwin = Toplevel(self)
+        newwin.geometry("300x150")
+        newwin.focus()
+        iid = self.test_table.focus()
+        selected = self.test_table.item(iid)['values']
+
+        entry = dict()
+        for key, value in zip(self.test_table['columns'], selected):
+                entry[key] = value
         
-            old_values = {
-                "Title": Treeview.item(select)['values'][0], 
-                "Username": Treeview.item(select)['values'][1], 
-                "Password": Treeview.item(select)['values'][2], 
-                "URL": Treeview.item(select)['values'][3], 
-                "Last_Modified": Treeview.item(select)['values'][4]
-                }
-            form_fields = {
-                "Title": "", 
-                "Username": "", 
-                "Password": "", 
-                "URL": "", 
-                "Last_Modified": ""
-            }
-            Label(newwin, text="Title").grid(row=0, column=0, padx=5)
-            titleentry = Entry(newwin, width = 25)
-            titleentry.grid(row=0, column=1, pady=5)
-            Label(newwin, text="Username").grid(row=1, column=0, padx=5)
-            userentry = Entry(newwin, width = 25)
-            userentry.grid(row=1, column=1, pady=5)
-            Label(newwin, text="Password").grid(row=2, column=0, padx=5)
-            passentry = Entry(newwin, width = 25)
-            passentry.grid(row=2, column=1, pady=5)
-            Label(newwin, text="URL").grid(row=3, column=0, padx=5)
-            urlentry = Entry(newwin, width = 25)
-            urlentry.grid(row=3, column=1, pady=5)
+        form_fields = dict()
+
         
-            #Obtains the user input and stores the values into a Dictionary entry to pass to Controller add_entry
-            def get_input():
+        Label(newwin, text="Title").grid(row=0, column=0, padx=5)
+        titleentry = Entry(newwin, width = 25)
+        titleentry.grid(row=0, column=1, pady=5)
+        Label(newwin, text="Username").grid(row=1, column=0, padx=5)
+        userentry = Entry(newwin, width = 25)
+        userentry.grid(row=1, column=1, pady=5)
+        Label(newwin, text="Password").grid(row=2, column=0, padx=5)
+        passentry = Entry(newwin, width = 25)
+        passentry.grid(row=2, column=1, pady=5)
+        Label(newwin, text="URL").grid(row=3, column=0, padx=5)
+        urlentry = Entry(newwin, width = 25)
+        urlentry.grid(row=3, column=1, pady=5)
+        
+        #Obtains the user input and stores the values into a Dictionary entry to pass to Controller add_entry
+        def get_input():
                 date = str(datetime.now())
                 form_fields["Title"] = titleentry.get()
                 form_fields["Username"] = userentry.get()
                 form_fields["Password"] = passentry.get()
                 form_fields["URL"] = urlentry.get()
                 form_fields["Last_Modified"] = date
+                
 
         
 
-            submitButton = ttk.Button(newwin, text="Submit", command= lambda:[get_input(),self.__ctrl.edit_entry(old_values, form_fields), self.show_info('Entry has been updated.'), newwin.destroy()])
-            submitButton.grid(row=5, column=0, pady= 5)
+        submitButton = ttk.Button(newwin, text="Submit", command= lambda:[get_input(),self.__ctrl.edit_entry(entry, form_fields), self.show_info('Entry has been updated.'), newwin.destroy()])
+        submitButton.grid(row=5, column=0, pady= 5)
 
-            cancelButton = ttk.Button(newwin, text="Cancel", command=lambda:[newwin.destroy()])
-            cancelButton.grid(row=5, column=2, pady= 5)
-        except:
-            self.show_error('No row has been selected.')
-            newwin.destroy()
+        cancelButton = ttk.Button(newwin, text="Cancel", command=lambda:[newwin.destroy()])
+        cancelButton.grid(row=5, column=2, pady= 5)
 
     #Deletes specified row from the table and removes its entry from the database
-    #UI needs work
-    def delete_entry(self, Treeview):
-        try:
-            newwin = Toplevel(self)
-            newwin.geometry("300x100")
-            newwin.focus()
-            select = Treeview.selection()[0]
-        
-            selected_values = {
-                "Title": Treeview.item(select)['values'][0], 
-                "Username":Treeview.item(select)['values'][1], 
-                "Password": Treeview.item(select)['values'][2], 
-                "URL":Treeview.item(select)['values'][3], 
-                "Last_Modified": Treeview.item(select)['values'][4]
-                }
-        
-            Label(newwin, text="Are you sure you want to delete this entry?").grid(row=0, column=0, padx=5, pady=5)
+    def delete_entry(self):
+        iid = self.test_table.focus()
+        selected = self.test_table.item(iid)['values']
+        entry = dict()
 
+        for key, value in zip(self.test_table['columns'], selected):
+                entry[key] = value
+
+        confirm = messagebox.askyesno(
+            title='Confirm',
+            message = 'Are you sure you want to delete this entry?'
+        )
+
+        if confirm:
+            self.ctrl.delete_entry(entry)
         
-
-            submitButton = ttk.Button(newwin, text="Submit", command= lambda:[self.__ctrl.delete_entry(selected_values), self.show_info('Entry has been deleted.'), newwin.destroy()])
-            submitButton.grid(row=1, column=0, pady= 5)
-
-            cancelButton = ttk.Button(newwin, text="Cancel", command=lambda:[newwin.destroy()])
-            cancelButton.grid(row=1, column=1, pady= 5)
-        except:
-            self.show_error('No row has been selected.')
-            newwin.destroy()
+            
 
    
     def show_error(self, message):
